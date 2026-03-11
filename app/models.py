36508b5a154ce6503de21277d921783607666257
fluffy-marmot <10621013+fluffy-marmot@users.noreply.github.com>
2026-03-10 20:56:36 -0700

    add Submission model to models.py, add total_submissions and registration_date fields to User model, tweak comments

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import time
 
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
@@ -9,15 +10,44 @@ db = SQLAlchemy()
 # not for database, just needed across different modules
 @dataclass
 class QueueItem:
-    submission_id: str  # unique id used by client to check status of their submission
-    timestamp: int  # Unix timestamp
-    code: str  # contents of submission's dictionary.c
-    header: str  # contents of submission's dictionary.h (blank if using distribution)
-    status: str = "pending"  # pending | running | done | error
-    output: str = ""  # output for webpage's "terminal"
+    submission_id: str          # unique id used by client to check status of their submission
+    timestamp: int              # Unix timestamp
+    code: str                   # contents of submission's dictionary.c
+    header: str                 # contents of submission's dictionary.h (blank if using distribution)
+    status: str = "pending"     # pending | running | done | error
+    output: str = ""            # output for webpage's "terminal"
 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), nullable=False)
+
+    total_submissions = db.Column(db.Integer, nullable=False, default=0)
+    registration_date = db.Column(db.BigInteger, nullable=False, default=lambda: int(time.time()))
+
+
+class Submission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # matches QueueItem.submission_id
+    submission_id = db.Column(db.String(36), unique=True, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user = db.relationship("User", backref=db.backref("submissions", lazy=True))
+
+    # Unix timestamp, user-defined label to differentiate their submissions
+    timestamp = db.Column(db.BigInteger, nullable=False, default=lambda: int(time.time()))
+    label = db.Column(db.String(100), nullable=True, default="")
+
+    # benchmark results
+    load_average    = db.Column(db.Float, nullable=False)
+    load_minimum    = db.Column(db.Float, nullable=False)
+    check_average   = db.Column(db.Float, nullable=False)
+    check_minimum   = db.Column(db.Float, nullable=False)
+    size_average    = db.Column(db.Float, nullable=False)
+    size_minimum    = db.Column(db.Float, nullable=False)
+    unload_average  = db.Column(db.Float, nullable=False)
+    unload_minimum  = db.Column(db.Float, nullable=False)
+    total_average   = db.Column(db.Float, nullable=False)
+    total_minimum   = db.Column(db.Float, nullable=False)
